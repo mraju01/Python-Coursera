2430347fbc6347c9843935ed7256dd5b43ccffdb
Maheswar <raju.mahesawr5@gmail.com>
2022-07-21 19:16:53 -0400

    Created a good structure

---
+++ Chapter 2 - Variables, expressions, and statements.py
@@ -0,0 +1,12 @@
+
+#Constants
+
+#Fixed Values such as number, letter and strings are called constants
+
+#string constants use single quotes ('') or ("")
+
+print (123) #Integer
+
+print(98.6) #float
+
+print("Hello World") #string
