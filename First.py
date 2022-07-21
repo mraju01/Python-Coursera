4015c199cd89a636fc59b8dee5e50133d0646100
Raju <mraju@citi-us.com>
2022-07-21 17:38:33 -0400

    first commit

---
+++ First.py
@@ -0,0 +1,20 @@
+#Conditional Steps
+x=2
+print (x)
+x=x+2
+print(x)
+
+#conditional steps
+x=5
+if x<10:
+    print ('smaller')
+if x>20:
+    print('Larger')
+print('Finish')
+
+#repeated Steps
+n=5
+while n>0:
+    print(n)
+    n=n-1
+print('Blast-Off')
---
+++ print.py
@@ -0,0 +1,4 @@
+msg = "Hello "
+name = "Mahesh"
+sum = msg + name
+print("Hello" + name)
