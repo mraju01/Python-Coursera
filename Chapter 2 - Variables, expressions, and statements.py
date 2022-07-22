b3860dae04bb006354dc64a40222b109c0994e01
Maheswar <raju.mahesawr5@gmail.com>
2022-07-21 21:00:44 -0400

    cch2

--- Chapter 2 - Variables, expressions, and statements.py
+++ Chapter 2 - Variables, expressions, and statements.py
@@ -6,7 +6,40 @@
 #string constants use single quotes ('') or ("")
 
 print (123) #Integer
-
 print(98.6) #float
-
 print("Hello World") #string
+
+#Variables
+#One of the most powerful features of a programming language is the ability to manipulate variables. A variable is a name that refers to a value.
+# **Note: Programmers generally choose names for their variables that are meaningful and
+# document what the variable is used for.**
+
+# 📌 They can contain both letters and numbers,
+# but **they cannot start with a number**. It is legal to use uppercase letters, but it is
+# a good idea to begin variable names with a lowercase letter (you’ll see why later).
+
+76trombones = 'big parade'
+SyntaxError: invalid syntax
+more@ = 1000000
+SyntaxError: invalid syntax
+class = 'Advanced Theoretical Zymurgy'
+SyntaxError: invalid syntax
+
+# NUmerical Experssion
+>>> xx = 2
+>>> xx = xx + 2
+>>> print(xx)
+4
+>>> yy = 440 * 12
+>>> print(yy)
+5280
+>>> zz = yy / 1000
+>>> print(zz)
+5.28
+
+>>> jj = 23
+>>> kk = jj % 5
+>>> print(kk)
+3
+>>> print(4 ** 3)
+64
